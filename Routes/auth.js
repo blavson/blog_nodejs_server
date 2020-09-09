5533cea26e4ac04b172604fd7ec7521fe2cf92ed
misha <blava1979@gmail.com>
2020-09-09 18:58:05 +0400

    fixed some route issues

--- Routes/auth.js
+++ Routes/auth.js
@@ -5,66 +5,69 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-router.post('/signup',  async(req, res) => {
+router.post('/signup',  async (req, res) => {
   let { username , email, password} = req.body;
-  const sql = "SELECT  name FROM users WHERE name =" + connection.escape(username);
-   connection.query(sql, (error, result, fields) => {
-    if (error) 
-      return res.status(400).send("DB Problem");
-    console.log(result)
-    if (result.length  > 0 ) {
-      return res.status(400).send("User exists");
-    }
+  let sql = "SELECT  name FROM users WHERE name =" + connection.escape(username);
+  try {
+  connection.query(sql, async (error, result, fields) => {
+   if (error) throw(error)
+      console.log(result)
+      if (result.length  > 0 ) {
+        return res.status(402).send("User exists");
+      }
   })  
-  
-    try {
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hash = bcrypt.hashSync(password, salt);
-      const sql = "INSERT INTO users VALUES(0, " + connection.escape(username) +"," 
-                                                 + connection.escape(email) + ","
-                                                 + "NOW(), " + connection.escape(hash) + ","
-                                                 + "NULL, NOW(), NOW())";
-      connection.query(sql, (error, result, fields) => {
-        if (error) throw(error);
-        console.log(result)
-      })                            
 
-      return res.sendStatus(200).send("Signed up")
-
-  } catch(err) {
-    console.log(err);
-  }
+   const salt = bcrypt.genSaltSync(saltRounds);
+   const hash = bcrypt.hashSync(password, salt);
+   const token =  jwt.sign({ email : email }, 'whatever');
+   sql = "INSERT INTO users VALUES(0, " + connection.escape(username) +"," 
+                                                      + connection.escape(email) + ","
+                                                      + "NOW(), " + connection.escape(hash) + ","
+                                                      + "NOW(), NOW(), 1," +  connection.escape(token) + ")";
+   connection.query(sql,   async (error, result, fields) => {
+    if (error) throw error
+     return res.header('Authorization','Bearer ' +  token).status(201).json({token : token, _id : result.insertId })
+    })
+   // return  res.setHeader('Authorization','Bearer ' +  token).send(token);
+    
+} catch(err) {
+  console.log(err)
+}
 })
+
+
 
   router.post('/login', async(req, res) => {
     let { email, password} = req.body
-    query = "SELECT * FROM users WHERE email =" + connection.escape(email);
+    query = "SELECT * FROM users WHERE email =" + connection.escape(email)
     connection.query(query, async (error, results, fields) => {
-    try {  
-        if (!results) 
-          res.sendStatus(401).send("email or password doesn't match") 
-        const match = await bcrypt.compare(password, results[0].password)
-        if (!match) 
-           res.sendStatus(402).send("password doesn't match") 
-         const _id = await results[0].id
-         const token =  jwt.sign({ email : email }, 'whatever');
-        return  res.header('Authorization','Bearer ' +  token).send({_id, token})
-    } catch (error) {
-        console.log(error)
-         res.sendStatus(407).send("Wrong email or password")
-    }     
+    try {
+         if (results.length === 0) {
+         return res.status(400).send("email  doesn't match") 
+         }
+         const match = await bcrypt.compare(password, results[0].password)
+         if (!match) 
+           return  res.status(400).send("password doesn't match") 
+        return  res.status(200).json({token:results[0].remember_token, _id : results[0].id})
+      } catch(error) {
+        return res.send(error)
+      }
     })
   })
 
+router.post('/logout', async(req, res) => {
+
+})
 
 
-
-router.get('/home',  (req, res) => {
-  jwt.verify(req.token, 'whatever', (err, data) => {
+router.post('/secure/verify',  async (req, res) => {
+   jwt.verify(req.body.token, 'whatever', (err, data) => {
       if (err) {
-        res.send.status(403)
+        console.log("Checking token failed")
+        res.status(403).send("invalid token")
   } else {
-      res.send(data)
+      console.log(data)
+      res.status(200).send(data)
   }
   })
 })
