87a3fe5e1a7c9f96b6c44bc7265f8f9604bb2ebd
misha <blava1979@gmail.com>
2020-09-10 00:28:48 +0400

    added lorem ipsum generated posts for user id:1

--- Routes/blog.js
+++ Routes/blog.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const { default: slugify } = require('slugify');
 const myAuth= require('./authorize')
+const  connection = require('../myconn')
+const LoremIpsum = require("lorem-ipsum").LoremIpsum
 
 router.get('/', (req, res) => {
   console.log("All blogs")
@@ -19,12 +21,47 @@ router.get('/', (req, res) => {
    const query = `SELECT * FROM posts WHERE slug ='${slug}'`;
  })
  
-  router.post('/', myAuth, (req, res) => {
+  router.post('/',  (req, res) => {
    console.log(req.body)
-   let {title, body, tags} = req.body
+   let {id, title, body, tags} = req.body
    slug=slugify(title)
-   const sql = `INSERT INTO posts (id, slug, body, tags) VALUES (1, "${slug}","${body}", "${tags}")`;
+   const sql = `INSERT INTO posts (id, slug, body, tags) VALUES (${id}, "${slug}","${body}", "${tags}")`;
+   console.log(sql);
+   connection.query(sql, async (error, result, fields) => {
+    if (result) 
+     return res.status(201).send("post added")
+    else 
+    return res.status(400).send("can't add post")
+   })
+  })
 
+  router.post('/loremipsum', (req, res) => {
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4
+      }
+    });
+    
+    console.log(lorem)
+
+   // const sql = "INSERT INTO posts (id, slug, body, tags) VALUES "
+    let s =''
+    for(let i = 0; i < 30; i++) {
+      const slug = slugify(lorem.generateWords(2))
+      const tags = lorem.generateWords(4)
+      const body =lorem.generateParagraphs(7)
+      // ${id}, "${slug}","${body}", "${tags}"`;
+      const sql = "INSERT INTO posts (id, slug, body, thumbnail, tags) VALUES (1," + connection.escape(slug) + "," + connection.escape(body) + ", " +  "'http://lorempixel.com/400/400/'" + "," + connection.escape(tags) +")"
+      connection.query(sql, async (error, result, fields) => {
+
+       })
+    }
+    return res.status(200).send("YEAH")
   })
  
 
